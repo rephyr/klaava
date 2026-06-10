5f49d2fe4d12cf6a3670f81b1a512b8121e8b5ae
Emilia Sipola <sipolaemiliaa@gmail.com>
2026-06-10 14:38:11 +0300

    add tests for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/test/setup.js',
+    setupFiles: './src/tests/setup.js',
   },
   server: {
     proxy: {
